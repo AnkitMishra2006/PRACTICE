9278f9c8bfe48f2d823fab4a9224fd9ca4f9da9a
Ankit Mishra <ankit.kumar.mishra2006@gmail.com>
2024-09-27 20:32:12 +0530

    first commit

---
+++ HTML AND CSS/1. Random Color Generator/app.js
@@ -0,0 +1,16 @@
+function getRandomColor(){
+    let red = Math.floor(Math.random()*255);
+    let green = Math.floor(Math.random()*255);
+    let blue = Math.floor(Math.random()*255);
+    let color = `rgb(${red}, ${green}, ${blue})`;
+    return color;
+}
+let btn = document.querySelector("button")
+btn.addEventListener("click", () => {
+    let h3 = document.querySelector("h3");
+    let div = document.querySelector("div")
+    let randomColor = getRandomColor();
+    h3.innerText = randomColor;
+    div.style.backgroundColor = randomColor;
+
+});
