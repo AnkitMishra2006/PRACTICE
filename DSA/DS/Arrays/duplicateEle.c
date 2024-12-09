33854810f6481963da72651312a8f85f21d436ac
Ankit Mishra <ankit.kumar.mishra2006@gmail.com>
2024-12-09 20:00:28 +0530

    Duplicate elements question done

--- DSA/DS/Arrays/duplicateEle.c
+++ DSA/DS/Arrays/duplicateEle.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 
+// Find duplicates in sorted array
 void duplicateEle(int a[], int length)
 {
     int i;
@@ -15,7 +16,48 @@ void duplicateEle(int a[], int length)
     
 }
 
+// Find duplicates in sorted array with their counts
+
+void duplicateEleCounts(int a[], int length)
+{
+    int i,j;
+    for(i = 0; i < length - 1; i++)
+    {
+        if(a[i] == a[i+1])
+        {
+            j = i + 1;
+            while(a[i] == a[j]) j++;
+            printf("%d is appearing %d times\n", a[i], j - i);
+            i = j - 1;
+        }
+    }
+}
+
+// Using Hashing
+void duplicates(int a[], int low, int high, int length)
+{
+    int i;
+    int h[high];
+    for(i = low; i <= high; i++)
+    {
+        h[i] = 0;
+    }
+    for(i = 0; i < length; i++)
+    {
+        h[a[i]]++;
+    }
+    for(i = low; i <= high; i++)
+    {
+        if(h[i] != 0)
+        {
+            printf("%d is appearing %d times\n", i, h[i]);
+        }
+    }
+}
+
 int main()
 {
+    int a[10] = {9,9,3,5,4,3,5,4,1,1};
+    duplicates(a, 1, 9, 10);
     return 0;
 }
