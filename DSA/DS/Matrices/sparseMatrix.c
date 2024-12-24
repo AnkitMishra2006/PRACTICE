7337b4270ef9498b74cfceee6e88c1498579e840
Ankit Mishra <ankit.kumar.mishra2006@gmail.com>
2024-12-24 17:07:05 +0530

    Sparse Add operation Done

--- DSA/DS/Matrices/sparseMatrix.c
+++ DSA/DS/Matrices/sparseMatrix.c
@@ -53,3 +53,32 @@ void display(struct Sparse s)
     }
     
 }
+struct Sparse* add(struct Sparse *s1, struct Sparse *s2)
+{
+    int p = 0, q = 0, k = 0;
+    if((s1->m!=s2->m) || (s1->n!=s2->n)) return 0;
+
+    struct Sparse *sum = (struct Sparse *) malloc(sizeof(struct Sparse));
+    sum->m = s1->m; sum->n = s1->n; 
+    sum->e = (struct element *) malloc((s1->num + s2->num)*sizeof(struct element));
+
+    while(p < s1->num && q < s2->num)
+    {
+        if(s1->e[p].i < s2->e[p].i) sum->e[k++] = s1->e[p++];
+        else if(s1->e[p].i > s2->e[p].i) sum->e[k++] = s2->e[q++];
+        else
+        {
+            if(s1->e[p].j < s2->e[p].j) sum->e[k++] = s1->e[p++];
+            else if(s1->e[p].j > s2->e[p].j) sum->e[k++] = s2->e[q++];
+            else
+            { 
+                sum->e[k] = s1->e[p++];
+                sum->e[k++].x += s2->e[q++].x;
+            }
+        }
+    }
+    for(; p < s1->num; p++) sum->e[k++] = s1->e[p];
+    for(; q < s1->num; q++) sum->e[k++] = s2->e[q];
+    sum->num = k;
+    return sum;
+}
