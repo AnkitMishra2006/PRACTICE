3bdcbb8c09ed1151c7fd0757385e6b2af303eb30
Ankit Mishra <ankit.kumar.mishra2006@gmail.com>
2024-12-29 08:42:30 +0530

    No need of TODO KG One

--- WebDev/With JS/ToDo KG coding/app.js
+++
@@ -1,8 +0,0 @@
-let todoList = [];
-
-function addTodo() {
-  let inputElement = document.querySelector("#todo-input");
-  let todoItem = inputElement.value;
-  todoList.push(todoItem);
-  inputElement.value = "";
-}
