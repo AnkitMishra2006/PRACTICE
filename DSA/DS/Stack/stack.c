5cfcc8c0c727567efb56c50342716b122ef518a2
Ankit Mishra <ankit.kumar.mishra2006@gmail.com>
2025-01-04 20:09:54 +0530

    Create function done

--- DSA/DS/Stack/stack.c
+++ DSA/DS/Stack/stack.c
@@ -8,6 +8,14 @@ typedef struct Stack
     int *s;
 } Stack;
 
+void create(Stack * st)
+{
+    printf("Enter the size of Stack: ");
+    scanf("%d", &st->size);
+    st->top = -1;
+    st->s = (int*) malloc(st->size * sizeof(int));
+
+}
 int main()
 {
 
