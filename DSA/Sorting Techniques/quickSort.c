6e16b9511974727cb67adde70e9797acf16f4263
Ankit Mishra <ankit.kumar.mishra2006@gmail.com>
2025-01-06 05:02:50 +0530

    Quick Sort Done

--- DSA/Sorting Techniques/quickSort.c
+++ DSA/Sorting Techniques/quickSort.c
@@ -16,7 +16,7 @@ void swap(int *x, int *y)
     *x = *y;
     *y = temp;
 }
-void partition(int a[], int l, int h)
+int partition(int a[], int l, int h)
 {
     int i = l, j = h, pivot = a[l];
 
@@ -28,23 +28,33 @@ void partition(int a[], int l, int h)
         } while (a[i] <= pivot);
         do
         {
-            j++;
+            j--;
         } while (a[j] > pivot);
         if (i < j)
         {
             swap(&a[i], &a[j]);
         }
     } while (i < j);
-    swap(&a[l], a[j]);
+    swap(&a[l], &a[j]);
     return j;
 }
 
+void QuickSort(int a[], int l, int h)
+{
+    int j;
+    if (l < h)
+    {
+        j = partition(a, l, h);
+        QuickSort(a, l, j);
+        QuickSort(a, j + 1, h);
+    }
+}
 int main()
 {
-    int a[] = {5, 6, 2, 9, 0, 1};
+    int a[] = {5, 6, 2, 9, 0, 1, INT_MAX};
     int n = sizeof(a) / sizeof(a[0]);
-    display(a, n);
-
-    display(a, n);
+    display(a, n - 1);
+    QuickSort(a, 0, 6);
+    display(a, n - 1);
     return 0;
 }
