de1923bf0861322cfe8106327e7f38399016a1c3
Ankit Mishra <ankit.kumar.mishra2006@gmail.com>
2025-01-14 04:34:23 +0530

    GET POST GET:id routes done

--- WebDev/Express and EJS/REST/index.js
+++ WebDev/Express and EJS/REST/index.js
@@ -1,16 +1,52 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Server working well");
-});
-
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-app.set(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public")));
+
+let posts = [
+  {
+    id: "1a",
+    username: "ankitmishra",
+    content: "Learning full stack web development",
+  },
+  {
+    id: "2b",
+    username: "deepakpal",
+    content: "Started self development",
+  },
+  {
+    id: "3c",
+    username: "priyanshu",
+    content: "started new series arcane loving it",
+  },
+];
+
+app.get("/posts", (req, res) => {
+  res.render("index.ejs", { posts });
+});
+
+app.get("/posts/:id", (req, res) => {
+  let { id } = req.params;
+  let post = posts.find((p) => id === p.id);
+  console.log(post);
+  res.render("show.ejs", { post });
+});
+
+app.get("/posts/new", (req, res) => {
+  res.render("new.ejs");
+});
+
+app.post("/posts", (req, res) => {
+  let { username, content } = req.body;
+  posts.push({ username, content });
+  res.redirect("/posts");
+});
 
 const port = 8080;
 app.listen(port, () => {
