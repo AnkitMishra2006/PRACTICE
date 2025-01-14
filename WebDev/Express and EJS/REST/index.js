06b031bcfbfeeb8ea0e48896aadfc32ebeba0a73
Ankit Mishra <ankit.kumar.mishra2006@gmail.com>
2025-01-15 01:57:20 +0530

    Edit route done quora REST

--- WebDev/Express and EJS/REST/index.js
+++ WebDev/Express and EJS/REST/index.js
@@ -2,7 +2,9 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const { v4: uuidv4 } = require("uuid");
+const methodOverride = require("method-override");
 
+app.use(methodOverride("_method"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -32,21 +34,40 @@ app.get("/posts", (req, res) => {
   res.render("index.ejs", { posts });
 });
 
-app.get("/posts/:id", (req, res) => {
-  let { id } = req.params;
-  let post = posts.find((p) => id === p.id);
-  res.render("show.ejs", { post });
-});
-
 app.get("/posts/new", (req, res) => {
   res.render("new.ejs");
 });
 
-app.post("/posts", (req, res) => {  
+app.post("/posts", (req, res) => {
   let { username, content } = req.body;
-  let id = uuid();
+  let id = uuidv4();
   posts.push({ id, username, content });
+  // console.log(id);
   res.redirect("/posts");
+});
+
+app.get("/posts/:id", (req, res) => {
+  let { id } = req.params;
+  console.log(id);
+  let post = posts.find((p) => id === p.id);
+  console.log(post);
+  res.render("show.ejs", { post });
+  // res.send("Request successful");
+});
+
+app.get("/posts/:id/edit", (req, res) => {
+  let { id } = req.params;
+  let post = posts.find((p) => id === p.id);
+  res.render("edit.ejs", { post });
+});
+
+app.patch("/posts/:id", (req, res) => {
+  let { id } = req.params;
+  let newContent = req.body.content;
+  let post = posts.find((p) => id === p.id);
+  post.content = newContent;
+  res.redirect("/posts");
+  console.log(id);
 });
 
 const port = 8080;
