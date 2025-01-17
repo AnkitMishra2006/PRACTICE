9e96901dd0e57997d89f1e9ec084619b95e8e84b
Ankit Mishra <ankit.kumar.mishra2006@gmail.com>
2025-01-18 03:41:36 +0530

    show create chats route created

--- WebDev/Databases/MongoDb/index.js
+++ WebDev/Databases/MongoDb/index.js
@@ -27,11 +27,38 @@ async function main() {
 
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
-
+// Root
 app.get("/", (req, res) => {
   res.send("Root is working working");
 });
 
+// All Chats
+app.get("/chats", async (req, res) => {
+  let chats = await Chat.find();
+  console.log(chats);
+  res.render("index.ejs", { chats });
+});
+
+// Form for New Chat
+app.get("/chats/new", (req, res) => {
+  res.render("new.ejs");
+});
+
+// Create Route Post
+app.post("/chats", async (req, res) => {
+  let { from, to, msg } = req.body;
+  let newChat = new Chat({
+    from: from,
+    msg: msg,
+    to: to,
+    created_at: new Date(),
+  });
+  newChat
+    .save()
+    .then((res) => console.log("New Chat Saved"))
+    .catch((err) => console.log(err));
+  res.redirect("/chats");
+});
 const port = 8080;
 
 app.listen(port, () => {
